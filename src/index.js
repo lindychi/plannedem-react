3d3ae1b544a2bfb88d96259b143766cdc1706f59
hanchi <igam0000@gmail.com>
2021-12-13 17:18:34 +0900

    0.5 login page update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./reset.css";
-import "./output.css";
 
 ReactDOM.render(
   <React.StrictMode>
