e5377a26673ac7a8f1eef900b667174e568bdf8e
lindychi <um0205@naver.com>
2022-03-16 05:25:48 +0900

    #0 onsnapshot update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { onAuthStateChanged } from "@firebase/auth";
-import { addDoc, collection, getDocs } from "@firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  updateDoc,
+} from "@firebase/firestore";
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { authService, dbService } from "./firebaseConfig";
@@ -14,23 +23,42 @@ function App() {
 
   const [todoList, setTodoList] = useState([]);
 
-  const getDataFromDb = async () => {
-    const todosSnapshot = await getDocs(collection(dbService, "todos"));
-    if (todosSnapshot.docs) {
-      setTodoList(todosSnapshot.docs.map((doc) => ({ ...doc.data() })));
-    }
-  };
-
   const onChangeTodoTitle = (e) => {
     e.preventDefault();
     setTodoTitle(e.target.value);
   };
-  const onClickAddTodo = async (e) => {
+  const onClickAddTodo = (e) => {
     e.preventDefault();
     addDoc(collection(dbService, "todos"), {
       title: todoTitle,
+      createdAt: dayjs().format(),
     });
     setTodoTitle("");
+  };
+  const onClickComplete = (e, todoId, todoComplete) => {
+    e.preventDefault();
+    updateDoc(doc(collection(dbService, "todos"), todoId), {
+      changedAt: dayjs().format(),
+      complete: todoComplete === false ? true : false,
+    });
+  };
+  const onClickWorking = (e, todoId, todoWorkArray) => {
+    e.preventDefault();
+    let newWorkArray = todoWorkArray ? todoWorkArray : [];
+
+    if (
+      newWorkArray.length > 0 &&
+      newWorkArray[newWorkArray.length - 1]["end"] === undefined
+    ) {
+      newWorkArray[newWorkArray.length - 1].end = dayjs().format();
+    } else {
+      newWorkArray.push({ start: dayjs().format() });
+    }
+
+    updateDoc(doc(collection(dbService, "todos"), todoId), {
+      changedAt: dayjs().format(),
+      workArray: newWorkArray,
+    });
   };
 
   onAuthStateChanged(authService, (user) => {
@@ -41,7 +69,19 @@ function App() {
     }
   });
 
-  getDataFromDb();
+  useEffect(() => {
+    const unsub = onSnapshot(
+      query(collection(dbService, "todos"), orderBy("createdAt", "desc")),
+      (snapshot) => {
+        const list = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTodoList(list);
+      }
+    );
+    return unsub;
+  }, [todoList]);
 
   return user === null ? (
     <Login />
@@ -56,11 +96,32 @@ function App() {
         value={todoTitle}
       />
       <button onClick={onClickAddTodo}>할 일 추가</button>
-      <ul>
-        {todoList?.map((todo) => (
-          <li>{todo.title}</li>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {todoList?.map((todo, index) => (
+          <div key={`${index}`} className="flex flex-row items-center">
+            {todo.title}
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4"
+              onClick={(e) => onClickWorking(e, todo.id, todo.workArray)}
+            >
+              {todo["workArray"] === undefined ? "시작" : "종료"}
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4"
+              onClick={(e) => onClickComplete(e, todo.id, todo.complete)}
+            >
+              {todo?.complete === false ? "미완료" : "완료"}
+            </button>
+            <div className="flex flex-col">
+              {todo?.workArray?.map((work) => (
+                <div>
+                  {work.start} ~ {work.end}
+                </div>
+              ))}
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
