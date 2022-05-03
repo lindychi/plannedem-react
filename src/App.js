f8e475b996aafa377a7cc64187fe4ceb57988d27
lindychi <um0205@naver.com>
2022-05-03 14:44:04 +0900

    #7 update complet

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
         <Login />
     ) : (
         <div>
-            <div class="flex items-center">
+            <div className="flex items-center">
                 <svg
-                    class="w-6 h-6 m-1"
+                    className="w-6 h-6 m-1"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
@@ -34,9 +34,9 @@ function App() {
                     }}
                 >
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M4 6h16M4 12h16M4 18h16"
                     ></path>
                 </svg>
@@ -46,16 +46,16 @@ function App() {
             <Drawer isOpen={open} setIsOpen={setOpen}>
                 <div className="flex">
                     <svg
-                        class="w-6 h-6"
+                        className="w-6 h-6"
                         fill="none"
                         stroke="currentColor"
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
                         ></path>
                     </svg>
