ae60a972d105b0fd9ab2a03efa84b3ff6c304fe9
lindychi <um0205@naver.com>
2022-05-03 10:32:59 +0900

    5 drawer (#6)
    * #5 other fils udpate
    
    * #5 add button component
    
    * #5 split todolist page code
    
    * #5 add drawer
    
    * #5 fix list updating
    
    * #5 update row pring

--- src/App.js
+++ src/App.js
@@ -1,99 +1,14 @@
 import { onAuthStateChanged } from "@firebase/auth";
-import {
-    addDoc,
-    collection,
-    deleteDoc,
-    doc,
-    onSnapshot,
-    orderBy,
-    query,
-    updateDoc,
-    where,
-} from "@firebase/firestore";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import { useRoutes } from "react-router-dom";
-import { authService, dbService } from "./firebaseConfig";
-import Login from "./pages/Login";
+import { useState } from "react";
+import { authService } from "./firebaseConfig";
 
-import routes from "./routes";
+import Login from "./pages/Login";
+import TodoList from "./pages/TodoList";
+import Drawer from "./components/Drawer";
 
 function App() {
-    let element = useRoutes(routes);
     const [user, setUser] = useState(authService.currentUser);
-    const [todoTitle, setTodoTitle] = useState("");
-
-    const [todoList, setTodoList] = useState([]);
-    const [iterTodoList, setIterTodoList] = useState([]);
-
-    const onChangeTodoTitle = (e) => {
-        e.preventDefault();
-        setTodoTitle(e.target.value);
-    };
-    const onClickAddTodo = (e) => {
-        e.preventDefault();
-        if (todoTitle.length > 0) {
-            addDoc(collection(dbService, "todos"), {
-                title: todoTitle,
-                createdAt: dayjs().format(),
-                complete: false,
-                user: user.uid,
-            });
-        }
-        setTodoTitle("");
-    };
-
-    /* 업무 완료 처리를 진행하는 함수 */
-    const onClickComplete = (e, todo) => {
-        e.preventDefault();
-        /* 반복 할일일 경우에는 추가할일을 만드는 작업을 함께 처리해주어야한다. */
-        if (todo?.iterTodoId !== undefined && todo.complete === false) {
-            const iterTodo = iterTodoList.find((iter) => {
-                return iter.id === todo.iterTodoId;
-            });
-            if (iterTodo) {
-                addDoc(collection(dbService, "todos"), {
-                    title: iterTodo.title,
-                    createdAt: dayjs()
-                        .add(iterTodo.iterValue, iterTodo.iterUnit)
-                        .format(),
-                    complete: false,
-                    user: user.uid,
-                    iterTodoId: iterTodo.id,
-                });
-            } else {
-                console.error(`'${iterTodo}'`, `'${todo.iterTodoId}'`);
-                return;
-            }
-        }
-        updateDoc(doc(collection(dbService, "todos"), todo.id), {
-            changedAt: dayjs().format(),
-            complete: todo.complete === false ? true : false,
-            completedAt: todo.complete === false ? dayjs().format() : undefined,
-        });
-    };
-    const onClickDelete = (e, todoId) => {
-        e.preventDefault();
-        deleteDoc(doc(collection(dbService, "todos"), todoId));
-    };
-    const onClickWorking = (e, todoId, todoWorkArray) => {
-        e.preventDefault();
-        let newWorkArray = todoWorkArray ? todoWorkArray : [];
-
-        if (
-            newWorkArray.length > 0 &&
-            newWorkArray[newWorkArray.length - 1]["end"] === undefined
-        ) {
-            newWorkArray[newWorkArray.length - 1].end = dayjs().format();
-        } else {
-            newWorkArray.push({ start: dayjs().format() });
-        }
-
-        updateDoc(doc(collection(dbService, "todos"), todoId), {
-            changedAt: dayjs().format(),
-            workArray: newWorkArray,
-        });
-    };
+    const [open, setOpen] = useState(false);
 
     onAuthStateChanged(authService, (user) => {
         if (user) {
@@ -103,113 +18,50 @@ function App() {
         }
     });
 
-    useEffect(() => {
-        if (user) {
-            const unsub = onSnapshot(
-                query(
-                    collection(dbService, "todos"),
-                    orderBy("createdAt", "desc"),
-                    where("user", "==", user.uid),
-                    where("createdAt", "<=", dayjs().format())
-                ),
-                (snapshot) => {
-                    const list = snapshot.docs.map((doc) => ({
-                        ...doc.data(),
-                        id: doc.id,
-                    }));
-                    setTodoList(list);
-                }
-            );
-            return unsub;
-        }
-    }, [todoList, user]);
-
-    useEffect(() => {
-        if (user) {
-            const unsub = onSnapshot(
-                query(
-                    collection(dbService, "iterTodos"),
-                    orderBy("createdAt", "desc"),
-                    where("user", "==", user.uid)
-                ),
-                (snapshot) => {
-                    const list = snapshot.docs.map((doc) => ({
-                        ...doc.data(),
-                        id: doc.id,
-                    }));
-                    setIterTodoList(list);
-                }
-            );
-            return unsub;
-        }
-    }, [iterTodoList, user]);
-
     return user === null ? (
         <Login />
     ) : (
         <div>
-            <div>{user.email}님</div>
-            <div>{element}</div>
-            <input
-                type="text"
-                placeholder="할 일을 입력해주세요."
-                onChange={onChangeTodoTitle}
-                value={todoTitle}
-            />
-            <button onClick={onClickAddTodo}>할 일 추가</button>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                {todoList
-                    ?.filter(
-                        (todo) =>
-                            todo.complete === false ||
-                            todo.completedAt >=
-                                dayjs().subtract(1, "day").format()
-                    )
-                    .map((todo, index) => (
-                        <div
-                            key={`${index}`}
-                            className="flex flex-row items-center"
-                        >
-                            {todo.title}
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4"
-                                onClick={(e) =>
-                                    onClickWorking(e, todo.id, todo.workArray)
-                                }
-                            >
-                                {todo["workArray"] === undefined ||
-                                todo["workArray"][todo["workArray"].length - 1]
-                                    .end !== undefined
-                                    ? "시작"
-                                    : "종료"}
-                            </button>
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4"
-                                onClick={(e) => onClickComplete(e, todo)}
-                                /* 반복되는 할일이 완료되었을 경우, 비활성화 다음 할일에서 관리하도록 한다. */
-                                disabled={
-                                    todo.complete === true &&
-                                    todo.iterTodoId !== undefined
-                                }
-                            >
-                                {todo?.complete === false ? "완료" : "미완료"}
-                            </button>
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4"
-                                onClick={(e) => onClickDelete(e, todo.id)}
-                            >
-                                삭제
-                            </button>
-                            <div className="flex flex-col">
-                                {todo?.workArray?.map((work, index) => (
-                                    <div key={`workIndex${index}`}>
-                                        {work.start} ~ {work.end}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
+            <div class="flex items-center">
+                <svg
+                    class="w-6 h-6 m-1"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => {
+                        setOpen(true);
+                    }}
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                </svg>
+                할 일 목록
             </div>
+            <TodoList user={user} />
+            <Drawer isOpen={open} setIsOpen={setOpen}>
+                <div className="flex">
+                    <svg
+                        class="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                        ></path>
+                    </svg>
+                    할 일 목록
+                </div>
+            </Drawer>
         </div>
     );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ children, onClick, disabled }) => {
+    return (
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xs p-2"
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
