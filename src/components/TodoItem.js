7116e51308120883049fba98a5806e44bb800ab8
lindychi <um0205@naver.com>
2022-05-12 22:42:35 +0900

    #19 print two time unit (#20)

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -18,16 +18,25 @@ const TodoItem = ({
             return "";
         }
 
-        const secTime = time / 1000;
-        if (secTime / (60 * 60 * 24) > 1) {
-            return parseInt(secTime / (60 * 60 * 24)) + "일";
-        } else if (secTime / 3600 > 1) {
-            return parseInt(secTime / (60 * 60)) + "시간";
-        } else if (secTime / 60 > 1) {
-            return parseInt(secTime / 60) + "분";
-        } else {
-            return parseInt(secTime) + "초";
+        let secTime = time / 1000;
+        let strArray = [];
+        if (secTime / (60 * 60 * 24) > 1 && strArray.length < 2) {
+            strArray.push(parseInt(secTime / (60 * 60 * 24)) + "일");
+            secTime = secTime % (60 * 60 * 24);
         }
+        if (secTime / 3600 > 1 && strArray.length < 2) {
+            strArray.push(parseInt(secTime / (60 * 60)) + "시간");
+            secTime = secTime % (60 * 60);
+        }
+        if (secTime / 60 > 1 && strArray.length < 2) {
+            strArray.push(parseInt(secTime / 60) + "분");
+            secTime = secTime % 60;
+        }
+        if (secTime > 0 && strArray.length < 2) {
+            strArray.push(parseInt(secTime) + "초");
+        }
+
+        return strArray.join(" ");
     };
 
     return (
